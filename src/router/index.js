4e95306a00ee5d74a1a6217c89c8f0895ce4d4c0
cook <1536529752@qq.com>
2019-08-03 10:32:35 +0800

    首页及欢迎页

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,31 @@
 import VueRouter from 'vue-router'
 import Vue from 'vue'
 import Login from '@/views/login'
+import Home from '@/views/home'
+import Welcome from '@/views/welcome'
 Vue.use(VueRouter)
 const router = new VueRouter({
   routes: [
+
     {
       path: '/login',
       name: 'login',
       component: Login
+
+    },
+    {
+      path: '/',
+      name: 'home',
+      component: Home,
+      redirect: '/welcome',
+      children: [
+        {
+          path: '/welcome',
+          name: 'welcome',
+          component: Welcome
+
+        }
+      ]
     }
   ]
 })
